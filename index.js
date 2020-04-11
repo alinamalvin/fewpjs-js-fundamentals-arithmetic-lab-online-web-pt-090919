92ead2c33fa2c50b23e4d4376a624bc22756855f
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-11 15:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
-let multiply=2*32
+let num1=
 
 // Create a variable called random that will generate a random integer greater than 0.
 // Create a variable called mod set to an equation whose remainder is equal to "4".
