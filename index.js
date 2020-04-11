57f0293e0e8874ac700f0dbe3fdc718b2906e66f
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-11 15:27:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ let multiply=num1*num2
 
 // Create a variable called random that will generate a random integer greater than 0.
 let random=Math.random()
+
 // Create a variable called mod set to an equation whose remainder is equal to "4".
+let mod=
 //Create a variable called max that will return "20" as the highest number in the set.
